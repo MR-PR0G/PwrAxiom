133c501e76c1ce70e5080caf40164f321e50ae91
Mr.Prog <mr.prog.offcial@gmail.com>
2026-07-06 15:12:01 +0330

    Delete src directory

--- src/monitor.h
+++
@@ -1,9 +0,0 @@
-#ifndef MONITOR_H
-#define MONITOR_H
-
-#include <gtk/gtk.h>
-
-GtkWidget* create_monitor_widget(void);
-const char* get_current_mode(void);
-
-#endif
--- src/power_core.h
+++
@@ -1,7 +0,0 @@
-#ifndef POWER_CORE_H
-#define POWER_CORE_H
-
-void pwraxiom_ensure_scripts(void);
-char* pwraxiom_get_script_path(const char* mode);
-
-#endif
